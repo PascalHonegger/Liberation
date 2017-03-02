1558407c8f5b37f3d5529dbb5e7f76dfa3b35fe8
Pascal Honegger <pascal.honegger@bluewin.ch>
2017-03-02 20:01:02 +0100

    NavMesh-Demo hinzugefügt

---
+++ Assets/Scripts/ClickMovePlayer.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+public class ClickMovePlayer : MonoBehaviour
+{
+	public NavMeshAgent AgentToMove;
+
+	private void OnMouseDown()
+	{
+		NavMeshHit hit;
+
+		var destination = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+		destination.y = 0;
+
+		AgentToMove.SetDestination(NavMesh.SamplePosition(destination, out hit,
+			4f, NavMesh.AllAreas)
+			? hit.position
+			: destination);
+	}
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.EventSystems;
+
+public class PlayerController : MonoBehaviour
+{
+	private const float NavMeshSampleDistance = 4f;
+
+	private NavMeshAgent _agent;
+
+	private void Start()
+	{
+		_agent = FindObjectOfType<NavMeshAgent>();
+	}
+
+	public void OnGroundClick(BaseEventData eventData)
+	{
+		var pointerEventData = (PointerEventData) eventData;
+
+		NavMeshHit hit;
+
+		_agent.SetDestination(NavMesh.SamplePosition(pointerEventData.pointerCurrentRaycast.worldPosition, out hit,
+			NavMeshSampleDistance, NavMesh.AllAreas)
+			? hit.position
+			: pointerEventData.pointerCurrentRaycast.worldPosition);
+
+		/*var destination = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+		destination.y = 0;
+		_agent.SetDestination(destination);*/
+	}
+
+	private void Update()
+	{
+		//TODO Maybe there is a better solution?
+		transform.eulerAngles = new Vector3(90, 0, 0);
+	}
+}
