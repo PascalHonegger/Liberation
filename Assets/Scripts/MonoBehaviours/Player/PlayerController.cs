efeae9aec0d1a3eed008efc3d076a8c49c5c0421
Pascal Honegger <pascal.honegger@edu.tbz.ch>
2017-03-20 19:40:43 +0100

    Beispiel eingebunden

---
+++ Assets/Scripts/MonoBehaviours/Player/PlayerController.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.EventSystems;
+
+public class PlayerController : MonoBehaviour
+{
+	public const string StartingPositionKey = "starting position";
+
+	private const float NavMeshSampleDistance = 4f;
+
+	private NavMeshAgent _agent;
+	private Interactable _currentInteractable;
+
+	private void Start()
+	{
+		_agent = FindObjectOfType<NavMeshAgent>();
+	}
+
+	public void OnGroundClick(BaseEventData eventData)
+	{
+		// The player is no longer headed for an interactable so set it to null.
+		_currentInteractable = null;
+
+		var pointerEventData = (PointerEventData) eventData;
+
+		NavMeshHit hit;
+
+		_agent.SetDestination(NavMesh.SamplePosition(pointerEventData.pointerCurrentRaycast.worldPosition, out hit,
+			NavMeshSampleDistance, NavMesh.AllAreas)
+			? hit.position
+			: pointerEventData.pointerCurrentRaycast.worldPosition);
+	}
+
+	// This function is called by the EventTrigger on an Interactable, the Interactable component is passed into it.
+	public void OnInteractableClick(Interactable interactable)
+	{
+		// Set the destination of the nav mesh agent to the found destination position and start the nav mesh agent going.
+		_agent.SetDestination(interactable.interactionLocation.position);
+
+		// Store the interactble that was clicked on.
+		_currentInteractable = interactable;
+	}
+
+	private void Update()
+	{
+		//TODO Maybe there is a better solution?
+		transform.eulerAngles = new Vector3(0, 0, 0);
+
+		//Interact with the interactable
+		if (_currentInteractable != null && !_agent.pathPending && Mathf.Approximately(_agent.remainingDistance, 0))
+		{
+			// Interact with the interactable and then null it out so this interaction only happens once.
+			_currentInteractable.Interact();
+			_currentInteractable = null;
+		}
+	}
+}
---
+++ Assets/Scripts/ScriptableObjects/Interaction/Reactions/ImmediateReactions/SceneReaction.cs
@@ -0,0 +1,31 @@
+﻿// The SceneReaction is used to change between scenes.
+// Though there is a delay while the scene fades out,
+// this is done with the SceneController class and so
+// this is just a Reaction not a DelayedReaction.
+
+public class SceneReaction : Reaction
+{
+	public string sceneName; // The name of the scene to be loaded.
+	public string startingPointInLoadedScene; // The name of the StartingPosition in the newly loaded scene.
+	public SaveData playerSaveData; // Reference to the save data asset that will store the StartingPosition.
+
+
+	private SceneController _sceneController;
+	// Reference to the SceneController to actually do the loading and unloading of scenes.
+
+
+	protected override void SpecificInit()
+	{
+		_sceneController = FindObjectOfType<SceneController>();
+	}
+
+
+	protected override void ImmediateReaction()
+	{
+		// Save the StartingPosition's name to the data asset.
+		playerSaveData.Save(PlayerController.StartingPositionKey, startingPointInLoadedScene);
+
+		// Start the scene loading process.
+		_sceneController.FadeAndLoadScene(this);
+	}
+}
