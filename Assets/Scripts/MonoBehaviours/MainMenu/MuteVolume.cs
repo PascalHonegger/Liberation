da72781d69a1ef7fe693b149d4c3302aec0b4a95
Pascal Honegger <pascal.honegger@edu.tbz.ch>
2017-04-04 14:43:31 +0200

    Startmenu um Mute-Knopf erweitert

---
+++ Assets/Scripts/MonoBehaviours/MainMenu/MuteVolume.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class MuteVolume : MonoBehaviour
+{
+	public Text VolumeActiveLabel;
+	public Text VolumeMutedLabel;
+	private AudioSource _audioToMute;
+
+	private void Start()
+	{
+		_audioToMute = FindObjectOfType<AudioSource>();
+		VolumeActive = true;
+	}
+
+	public void SwitchState()
+	{
+		VolumeActive = !VolumeActive;
+	}
+
+	private bool VolumeActive
+	{
+		get { return _audioToMute.mute; }
+		set
+		{
+			_audioToMute.mute = value;
+			if (_audioToMute.mute)
+			{
+				VolumeActiveLabel.enabled = true;
+				VolumeMutedLabel.enabled = false;
+				_audioToMute.mute = false;
+			}
+			else
+			{
+				VolumeActiveLabel.enabled = false;
+				VolumeMutedLabel.enabled = true;
+				_audioToMute.mute = true;
+			}
+		}
+	}
+}
